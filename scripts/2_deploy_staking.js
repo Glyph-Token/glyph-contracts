3055fe52c63ba486e022e44dbf643d6e58b05aae
beyond7 <bs.beyonddev@gmail.com>
2021-02-24 09:26:11 -0500

    mainnet token addr

--- scripts/2_deploy_staking.js
+++ scripts/2_deploy_staking.js
@@ -2,7 +2,7 @@ const hre = require('hardhat');
 const moment = require('moment');
 
 async function main() {
-  const glyphAddress = '0x2EbD9D1A522C766E982Fddb3D9aED1300fC694d9';
+  const glyphAddress = '0x299948bc2CA54a5e814B19849327A6d9a0e7de1b';
   const startTime = moment().add(1, 'hours').unix();
 
   const Staking = await hre.ethers.getContractFactory('Staking');
