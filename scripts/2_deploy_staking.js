48fa8059ff16a7cc57a5419bb82ea82daf9f1b78
beyond7 <bs.beyonddev@gmail.com>
2021-02-25 07:15:02 -0500

    script update

--- scripts/2_deploy_staking.js
+++ scripts/2_deploy_staking.js
@@ -3,7 +3,7 @@ const moment = require('moment');
 
 async function main() {
   const glyphAddress = '0x299948bc2CA54a5e814B19849327A6d9a0e7de1b';
-  const startTime = moment().add(1, 'hours').unix();
+  const startTime = moment().add(4, 'hours').unix();
 
   const Staking = await hre.ethers.getContractFactory('Staking');
   staking = await Staking.deploy(glyphAddress, startTime);
